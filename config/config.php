95ba0df9c26f3a32bde8235d30af1412076dd150
ad.kaes <antoine.driget@gmail.com>
2023-11-27 19:48:23 +0000

    Ajout du projet Nextcloud

---
+++ config/config.php
@@ -0,0 +1,22 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc3a4dnnzqr6',
+  'passwordsalt' => 'Mbb29wJDFfD6XXuoA7nxC55WsfGzdP',
+  'secret' => 'LQbRQQ8ebSXbKx+/6eQwyuCe9b6DnzJ25FA/vJ6d+ux1LkIn',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.133.162',
+  ),
+  'datadirectory' => '/var/www/html/nextcloud/data',
+  'dbtype' => 'mysql',
+  'version' => '27.1.3.2',
+  'overwrite.cli.url' => 'http://192.168.133.162/nextcloud',
+  'dbname' => 'nextcloud_db',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud_user',
+  'dbpassword' => 'Respons11@!',
+  'installed' => true,
+);
